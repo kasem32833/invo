94e610d3685f1a83ffc66f0183703a87d9f7ba63
kasem32833 <abulkasem32833@gmail.com>
2023-03-16 13:56:31 +0300

    add jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,16 @@
 import './bootstrap';
 
+$ = window.$ = window.jQuery = require( "jquery" );
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+jQuery(document).ready(function($)){
+   setTimeout(function () {
+    $('#status-message').slideUp('slow')
+    }, 2000);
+}

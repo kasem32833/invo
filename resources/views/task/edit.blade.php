bcca0fa4b459594461f0ee35d525e6e0e7057ef0
kasem32833 <abulkasem32833@gmail.com>
2023-03-19 16:51:54 +0300

    Update edit.blade.php

--- resources/views/task/edit.blade.php
+++ resources/views/task/edit.blade.php
@@ -2,113 +2,65 @@
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Edit Client') }}
+                {{ __('Edit Task') }}
             </h2>
-            <a href="{{ route('client.index') }}"
+            <a href="{{ route('task.index') }}"
                 class="border border-indigo-700 bg-indigo-500 hover:bg-indigo-700 rounded text-white px-4 py-2 shadow">Back</a>
         </div>
     </x-slot>
-
-
-
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{-- create new client --}}
-                    <form action="{{route('client.update', $client->id)}}" method="POST" enctype="multipart/form-data">
+                    <form action="{{route('task.update', $task->id )}}" method="POST" enctype="multipart/form-data">
                         @csrf
-                        @method('PUT')
                         <div class="mt-6 flex space-x-4">
                             <div class="flex-1">
                                 <label for="name" class="formLabel">Name</label>
-                                <input type="text" name="name" id="name" class="formInput" value="{{$client->name}}">
+                                <input type="text" name="name" id="name" class="formInput" value="{{ $task->name }}">
                                 @error('name')
-                                    <p class="text-red-700 text-sm">{{$message}}</p>
-                                @enderror
-                            </div>
-
-                            <div class="flex-1">
-                                <label for="username" class="formLabel">User Naame</label>
-                                <input type="text" name="username" id="username" class="formInput" value="{{ $client->username}}">
-                                @error('username')
                                     <p class="text-red-700 text-sm">{{$message}}</p>
                                 @enderror
                             </div>
                         </div>
                         <div class=" flex mt-6 space-x-4">
                             <div class="flex-1">
-                                <label for="email" class="formLabel">Email</label>
-                                <input type="text" name="email" id="email" class="formInput" value="{{ $client->email}}">
-                                @error('email')
+                                <label for="price" class="formLabel">price</label>
+                                <input type="number" name="price" id="price" class="formInput py-2" value="{{ $task->price }}">
+                                @error('price')
                                     <p class="text-red-700 text-sm">{{$message}}</p>
                                 @enderror
                             </div>
                             <div class="flex-1">
-                                <label for="phone" class="formLabel">Phone</label>
-                                <input type="text" name="phone" id="phone" class="formInput" value="{{ $client->phone }}">
-                                @error('phone')
+                                <label for="client_id" class="formLabel">Client Name</label>
+                                <select name="client_id" id="client_id" class="formInput">
+                                    <option value="none" >Select Client</option>
+
+                                    {{-- for dynamic country --}}
+                                    @foreach ($clients as $client)
+                                        <option value="{{$client->id}} {{$client->id == old('client_id') ? 'selected': ''}}">{{$client->name}}</option>
+                                    @endforeach
+                                </select>
+                                @error('client_id')
                                     <p class="text-red-700 text-sm">{{$message}}</p>
                                 @enderror
                             </div>
                         </div>
                         <div class=" flex space-x-2 mt-6 justify-between">
                             <div class="flex-1">
-                                <label for="country" class="formLabel">Country</label>
-
-
-
-                                <select name="country" id="" class="formInput">
-                                    <option value="none" >Select Country</option>
-                                    {{-- for dynamic country --}}
-                                    @foreach ($countries as $country)
-                                        <option value=" {{$country}}" {{ $client->country == $country ? 'selected' : '' }}>{{$country}}</option>
-                                    @endforeach
-                                </select>
-                                @error('country')
+                                <label for="description" class="formLabel">Description</label>
+                                <textarea name="description" id="description"  rows="10" class="formInput">
+                                    {{old('description')}}
+                                </textarea>
+                                @error('description')
                                 <p class="text-red-700 text-sm">{{$message}}</p>
                                 @enderror
-                            </div>
-
-                            <div class="flex-1">
-                                <label for="status" class="formLabel">Status</label>
-                                <select name="status" id="status" class="formInput">
-                                    <option value="active" {{ $client->status == 'active'? 'selected': ''}}>Active</option>
-                                    <option value="inactive" {{$client->status == 'inactive'? 'selected': ''}}>In Active</option>
-                                </select>
-                                @error('status')
-                                <p class="text-red-700 text-sm">{{$message}}</p>
-                                @enderror
-                            </div>
-                            <div class="w-48">
-                                <label for="">Select Your Image</label>
-                                <label for="thumbnail" class="formLabel border border-green-500 px-4 py-2 text-md-center">Image</label>
-                                <input type="file" name="thumbnail" id="thumbnail" class="formInput hidden">
-                                @error('thumbnail')
-                                <p class="text-red-700 text-sm">{{$message}}</p>
-                                @enderror
-                            </div>
-                            {{-- profile image showing --}}
-
-
-                            {{-- profile image url select --}}
-
-                                @php
-                                function getImageUrl($image){
-                                    if(str_starts_with($image, 'http')){
-                                        return $image;
-                                    }
-                                    return asset('storage/uploads') . '/'. $image;
-                                }
-                                @endphp
-
-                            <div class="w-32 ">
-                                <img class="rounded-full"  width="50" height="50" src="{{getImageUrl($client->thumbnail)}}" alt="">
                             </div>
                         </div>
                         <div class="mt-6">
-                            <button type="submit" class="submit-button">Update</button>
+                            <button type="submit" class="submit-button">Submit</button>
                         </div>
                     </form>
                 </div>
